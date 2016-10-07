0e863a41b599da527bb0449bf611a66d5003896d
Huy Nguyen <nguyen.quanghuy@mnltechnology.com>
2016-10-07 15:55:49 +0800

    Init project

---
+++ CarPrize/RegionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPrize
+{
+    public enum RegionType
+    {
+        Europe = 1,
+        USA = 2,
+        Japan = 3
+    }
+}
