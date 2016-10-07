730e07552f8363b8f304edcdcd7a4a059c023b0d
Huy Nguyen <nguyen.quanghuy@mnltechnology.com>
2016-10-07 16:01:12 +0800

    Init project

---
+++ CarPrice.Test/CarPriceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace CarPrize.Test
+{
+    [TestFixture]
+    public class CarPriceTest
+    {
+        [SetUp]
+        public void Init()
+        {
+            //Car.InitData();
+        }
+
+        [Test]
+        [TestCase(6.0f, 217900f, "Europe", ExpectedResult = 34410768f)]
+        [TestCase(1.5f, 19490f, "Japan", ExpectedResult = 1744121.12f)]
+        [TestCase(3.6f, 36995f, "USA", ExpectedResult = 3700091.92f)]
+        [TestCase(1.0f, 6000f, "China", ExpectedResult = 315840f)]
+        public float CarPrize(float liter, float originalPrice, string region)
+        {
+            Car car = new Car()
+            {
+                Liter = liter,
+                OriginalPrice = originalPrice,
+                Region = region
+            };
+            return car.GetPrice();
+        }
+    }
+}
---
+++ CarPrice/CarType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CarPrize
+{
+    public enum CarType
+    {
+        Under2L = 1,
+        From2LToUnder5L = 2,
+        GreaterThan5L = 3
+    }
+}
