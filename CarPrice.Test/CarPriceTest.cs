4f9e29f0e89b6f33f9029159b99a02ddae80448c
Huy Nguyen <nguyen.quanghuy@mnltechnology.com>
2017-01-03 15:20:17 +0800

    Update Unittest

--- CarPrice.Test/CarPriceTest.cs
+++ CarPrice.Test/CarPriceTest.cs
@@ -9,7 +9,7 @@ namespace CarPrize.Test
         [TestCase(6.0f, 217900f, "Europe", ExpectedResult = 34410768f)]
         [TestCase(1.5f, 19490f, "Japan", ExpectedResult = 1744121.12f)]
         [TestCase(3.6f, 36995f, "USA", ExpectedResult = 3700091.92f)]
-        [TestCase(1.0f, 6000f, "China", ExpectedResult = 315840f)]
+        [TestCase(1.0f, 6000f, "China", ExpectedResult = Assert.Throws<>)]
         public float CarPrize(float liter, float originalPrice, string region)
         {
             ICar car = new Car()
